cb0617dbf8876756a1e28307a356e95363d4e4c6
GimYoungPhil <youngphil.gim@gmail.com>
2018-02-01 01:10:46 +0900

    add modules/idols

---
+++ src/store/modules/idols.js
@@ -0,0 +1,33 @@
+import API from '../../api'
+import * as types from '../mutation-types'
+
+const state = {
+  isFetching: false,
+  list: [],
+}
+const getters = {
+}
+const actions = {
+  fetchIdols ({ commit }) {
+    commit(types.REQUEST_IDOLS)
+    API.getIdols()
+       .then(idols => commit(types.RECEIVE_IDOLS, { idols }))
+  }
+}
+const mutations = {
+  [types.REQUEST_IDOLS] (state) {
+    state.isFetching = true
+    state.list = []
+  },
+  [types.RECEIVE_IDOLS] (state, { idols }) {
+    state.isFetching = false
+    state.list = idols
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+}
