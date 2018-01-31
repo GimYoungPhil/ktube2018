37f8e0fef38f48ac76c679607efdb1bac59ba732
GimYoungPhil <youngphil.gim@gmail.com>
2018-02-01 01:11:04 +0900

    add Idols.vue

---
+++ src/components/Idols.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    idols
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  computed: mapState([
+    'idols'
+  ]),
+  methods: {
+    ...mapActions([
+      'fetchIdols'
+    ])
+  },
+  mounted() {
+    
+    this.fetchIdols()
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Foo from './components/Foo.vue'
 import Bar from './components/Bar.vue'
+import Idols from './components/Idols.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/bar', component: Bar },
+  { path: '/idols', component: Idols },
 ]
 
 const router = new VueRouter({
