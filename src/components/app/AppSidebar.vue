3bfc4f71735a459cdc1088474b20277d3837a048
GimYoungPhil <youngphil.gim@gmail.com>
2018-03-25 03:19:20 +0900

    add AppSidebar

---
+++ src/components/app/AppSidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sidebar" id="side">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
